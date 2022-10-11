dfd9d346333f427b6f2e8b14408254af41592c6d
Roma1125 <ramazonomonturdiyev17@gmail.com>
2022-10-11 11:09:01 +0500

    if06.py

--- if06.py
+++ if06.py
@@ -6,4 +6,20 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    a=n//10000
+    b=(n//1000)%10
+    c=(n//100)%10
+    s=(n//10)%10
+    q=(n%10)
+    if a>b and a>c and a>s and a>q:
+        d=1
+    elif b>a and b>c and b>s and b>q:
+        d=2
+    elif c>a and c>b and c>s and c>q:
+        d=3
+    elif s>a and s>b and s>c and s>q:
+        d=4
+    elif q>a and q>b and q>c and q>s:
+        d=5
+    return d
+print (main(12345))
